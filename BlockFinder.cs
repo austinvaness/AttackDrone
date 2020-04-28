c81e6d34d76e076e643cc4e31ae21e13f93ffbc5
austinvaness <austinvaness@gmail.com>
2020-04-27 22:13:22 -0500

    Initial commit

---
+++ BlockFinder.cs
@@ -0,0 +1,42 @@
+﻿using Sandbox.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        #region Block Finder
+        T GetBlock<T> (string name = null) where T : class, IMyTerminalBlock
+        {
+            List<T> blocks = new List<T>();
+            if (string.IsNullOrEmpty(name))
+                GridTerminalSystem.GetBlocksOfType(blocks, b => b.CubeGrid.EntityId == Me.CubeGrid.EntityId);
+            else
+                GridTerminalSystem.GetBlocksOfType(blocks, b => b.CubeGrid.EntityId == Me.CubeGrid.EntityId && b.CustomName == name);
+            return blocks.FirstOrDefault();
+
+        }
+        List<T> GetBlocks<T> (string groupName = null) where T : class, IMyTerminalBlock
+        {
+            List<T> blocks;
+            if (string.IsNullOrEmpty(groupName))
+            {
+                blocks = new List<T>();
+                GridTerminalSystem.GetBlocksOfType(blocks, b => b.CubeGrid.EntityId == Me.CubeGrid.EntityId);
+            }
+            else
+            {
+                IMyBlockGroup group = GridTerminalSystem.GetBlockGroupWithName(groupName);
+                if (group == null)
+                    return new List<T>(0);
+                blocks = new List<T>();
+                group.GetBlocksOfType(blocks, b => b.CubeGrid.EntityId == Me.CubeGrid.EntityId);
+            }
+            return blocks;
+
+        }
+        #endregion
+
+    }
+}
---
+++ Clock.cs
@@ -0,0 +1,68 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public class Clock
+        {
+            public int Runtime
+            {
+                get; private set;
+            }
+            private readonly double secondsPerTick;
+
+            public Clock(UpdateFrequency frequency)
+            {
+                Runtime = 0;
+                switch (frequency)
+                {
+                    case (UpdateFrequency.Update1):
+                        secondsPerTick = 1.0 / 60;
+                        break;
+                    case (UpdateFrequency.Update10):
+                        secondsPerTick = 1.0 / 6;
+                        break;
+                    case (UpdateFrequency.Update100):
+                        secondsPerTick = 5.0 / 3;
+                        break;
+                }
+            }
+
+            public void Update ()
+            {
+                Runtime++;
+                if (Runtime >= int.MaxValue)
+                    Runtime = 0;
+            }
+
+            public double GetSeconds (int start)
+            {
+                int diff = Runtime - start;
+                return diff * secondsPerTick;
+            }
+
+            public int GetTick (double seconds)
+            {
+                double ticks = seconds / secondsPerTick;
+                int roundedTicks = (int)Math.Ceiling(ticks);
+                return Runtime + roundedTicks;
+            }
+        }
+    }
+}
